6ca1874d8b5507c0ef05e6aaba59bc4c607f2d96
Bruce Buckland <bruce@odellventures.com>
2016-10-31 04:42:37 -0400

    Swift 3 for CUPIdPSignInProvider

---
+++ compilecup/compilecup.h
@@ -0,0 +1,19 @@
+//
+//  compilecup.h
+//  compilecup
+//
+//  Created by Bruce Buckland on 10/31/16.
+//  Copyright © 2016 Amazon Web Services. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for compilecup.
+FOUNDATION_EXPORT double compilecupVersionNumber;
+
+//! Project version string for compilecup.
+FOUNDATION_EXPORT const unsigned char compilecupVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <compilecup/PublicHeader.h>
+
+
